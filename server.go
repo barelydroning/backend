9cf3526748a9d95f466d75224bedc9f68d3fbbcc
axelri <axel.riese@gmail.com>
2017-11-15 21:24:01 +0100

    send and receive data

--- server.go
+++ server.go
@@ -7,9 +7,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
+
+type sensorData struct {
+	pitch    float64
+	roll     float64
+	azimuth  float64
+	altitude float64
+}
 
 var addr = flag.String("addr", ":8080", "http service address")
 
@@ -22,24 +30,39 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+func inputHandler(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+		}
+		if messageType == websocket.TextMessage {
+			fmt.Println(string(p))
+		} else {
+			log.Println("Byte message not supported")
+		}
+	}
+}
+
+func outputHandler(conn *websocket.Conn) {
+	ticker := time.NewTicker(2000 * time.Millisecond)
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("Sending data...")
+			conn.WriteMessage(websocket.TextMessage, []byte("Hello!"))
+		}
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
+	go inputHandler(conn)
+	go outputHandler(conn)
 }
 
 func main() {
